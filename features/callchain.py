28d0ebaf80576fe36bb5126c924a4717796727be
Heersin <teablearcher@gmail.com>
2022-05-10 22:26:48 +0800

    lightly update chain display

--- features/callchain.py
+++ features/callchain.py
@@ -2,6 +2,14 @@ from .src.callchain import main
 from binaryninja.interaction import AddressField, get_form_input
 
 
+def print_chain(chain_list):
+    print("CHAIN: ")
+    for item in chain_list:
+        print(hex(item), end='')
+        print('->', end='')
+    print("")
+
+
 def recipes_callchain(bv):
     source_f = AddressField('src addr (default entry)', default=0)
     sink_f = AddressField('sink addr', default=0)
@@ -15,4 +23,4 @@ def recipes_callchain(bv):
         return
 
     call_chain = [hex(i) for i in main(bv, sink_addr, src_addr)]
-    print(call_chain)
+    print_chain(call_chain)
